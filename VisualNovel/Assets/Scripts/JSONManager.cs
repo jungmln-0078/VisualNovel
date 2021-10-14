ea98e146852ceeb997ac028cd17b36e107caa2bb
XeAoDev <jungmln0078@naver.com>
2021-10-14 13:01:59 +0900

    Project Setting

---
+++ VisualNovel/Assets/Scripts/JSONManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace VisualNovelXML
+{
+    class JSONManager
+    {
+        public static string Locale { get; set; }
+        public static object GetLocale()
+        {
+            StreamReader sr = new(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + $"/{Locale}.json");
+            string jsonString = sr.ReadToEnd();
+            object json = JsonConvert.DeserializeObject(jsonString);
+            return json;
+        }
+    }
+}
